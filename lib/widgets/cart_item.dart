5c7d852e7833d50521fe8cb93a85fe0cab566e34
Abd-Elrahman Elsayed <41774422+GeekAbdou@users.noreply.github.com>
2021-03-29 01:55:52 +0200

    Adding Alert Before Deleting Item

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -40,6 +40,29 @@ class CartItem extends StatelessWidget {
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text("Are you sure ?!"),
+            content: Text("Are you sure to remove this item ?"),
+            actions: [
+              FlatButton(
+                child: Text("No"),
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+              ),
+              FlatButton(
+                child: Text("Yes!"),
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
       child: Card(
         margin: EdgeInsets.symmetric(
           horizontal: 15,
