dddd0e8cc622caca97891c4e2458f6bb7016e82a
Abd-Elrahman Elsayed <41774422+GeekAbdou@users.noreply.github.com>
2021-03-30 01:42:31 +0200

    deleting products works now

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../screens/edit_product_screen.dart';
 import '../providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
 
   UserProductItem(
+    this.id,
     this.title,
     this.imageUrl,
   );
@@ -31,7 +34,9 @@ class UserProductItem extends StatelessWidget {
               IconButton(
                 icon: Icon(Icons.delete),
                 color: Theme.of(context).errorColor,
-                onPressed: () {},
+                onPressed: () {
+                  Provider.of<Products>(context, listen: false).deleteProduct(id);
+                },
               ),
             ],
           ),
