c01c491423dc1a576348c166a506cc3c741c579e
Abd-Elrahman Elsayed <41774422+GeekAbdou@users.noreply.github.com>
2021-03-30 23:57:19 +0200

    deleting and editing
    now  in fire base works fine 
    exception handling for errors in deleting and editing

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -9,37 +9,47 @@ class UserProductItem extends StatelessWidget {
   final String title;
   final String imageUrl;
 
-  UserProductItem(
-    this.id,
-    this.title,
-    this.imageUrl,
-  );
+  UserProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
-    final ProductData = Provider.of<Products>(context);
+    final scaffold = Scaffold.of(context);
     return ListTile(
-        title: Text(title),
-        leading: CircleAvatar(
-          backgroundImage: NetworkImage(imageUrl),
+      title: Text(title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamed(EditProductScreen.routeName, arguments: id);
+              },
+              color: Theme.of(context).primaryColor,
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () async {
+                try {
+                  await Provider.of<Products>(context, listen: false)
+                      .deleteProduct(id);
+                } catch (error) {
+                  scaffold.showSnackBar(
+                    SnackBar(
+                      content: Text('Deleting failed!', textAlign: TextAlign.center,),
+                    ),
+                  );
+                }
+              },
+              color: Theme.of(context).errorColor,
+            ),
+          ],
         ),
-        trailing: Container(
-          width: 100,
-          child: Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.edit),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.delete),
-                color: Theme.of(context).errorColor,
-                onPressed: () {
-                  Provider.of<Products>(context, listen: false).deleteProduct(id);
-                },
-              ),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
