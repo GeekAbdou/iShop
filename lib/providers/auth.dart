c6f27cb946b3ccad11369e5b8babc4cb783f2c44
Abd-Elrahman Elsayed <41774422+GeekAbdou@users.noreply.github.com>
2021-04-02 22:00:20 +0200

    login auth added

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,23 +1,34 @@
 import 'dart:convert';
-import 'dart:async';
-import 'package:flutter/material.dart';
+
+import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 class Auth with ChangeNotifier {
-    String _token;
-    DateTime _expiryDate;
-    String _userId;
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=AIzaSyAGmVYt26GWSwkyHhc5JbbTKv1ESHM_kNE';
+    final response = await http.post(
+      url,
+      body: json.encode(
+        {
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        },
+      ),
+    );
+    print(json.decode(response.body));
+  }
 
   Future<void> signup(String email, String password) async {
-      const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[AIzaSyAGmVYt26GWSwkyHhc5JbbTKv1ESHM_kNE]';
-      http.post(
-        url,
-        body: jsonEncode({
-          'email' : email,
-          'password' : password,
-          'returnSecureToken' : true,
-
-        })
-      );
-    }
+    return _authenticate(email, password, 'signupNewUser');
   }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'verifyPassword');
+  }
+}
