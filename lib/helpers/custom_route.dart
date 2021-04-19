df2a725e31fface0d53d7342d9ffef7c31b7e192
Abd-Elrahman Elsayed <41774422+GeekAbdou@users.noreply.github.com>
2021-04-19 11:19:40 +0200

    updating settings.name due to flutter update

--- lib/helpers/custom_route.dart
+++ lib/helpers/custom_route.dart
@@ -16,7 +16,7 @@ class CustomRoute<T> extends MaterialPageRoute<T> {
     Animation<double> secondaryAnimation,
     Widget child,
   ) {
-    if (settings.isInitialRoute) {
+    if (settings.name == '/') {
       return child;
     }
     return FadeTransition(
@@ -35,7 +35,7 @@ class CustomPageTransitionBuilder extends PageTransitionsBuilder {
     Animation<double> secondaryAnimation,
     Widget child,
   ) {
-    if (route.settings.isInitialRoute) {
+    if (route.settings.name == '/') {
       return child;
     }
     return FadeTransition(
